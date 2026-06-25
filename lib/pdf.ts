99ac78e95ada4eefcac81532405905dd153d60de
Mohamed Arreh <arrehmoh40@gmail.com>
2026-06-25 15:50:14 -0400

    Swap pdf-parse for unpdf to fix serverless PDF parsing

--- lib/pdf.ts
+++ lib/pdf.ts
@@ -1,10 +1,7 @@
+import { extractText, getDocumentProxy } from 'unpdf'
+
 export async function extractPdfText(data: Uint8Array): Promise<string> {
-  const { PDFParse } = await import('pdf-parse')
-  const parser = new PDFParse({ data })
-  try {
-    const result = await parser.getText({ pageJoiner: '\n\n' })
-    return result.text.replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F]/g, '')
-  } finally {
-    await parser.destroy()
-  }
+  const pdf = await getDocumentProxy(data)
+  const { text } = await extractText(pdf, { mergePages: true })
+  return text.replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F]/g, '')
 }
